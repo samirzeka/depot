d43d3210d13dfcf5ff28855aa138c995ef7c09c9
samirzeka <sam_ned00@hotmail.com>
2012-02-20 16:47:24 +0100

    Toevoeging controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Depot::Application.routes.draw do
+  resources :line_items
+
+  resources :carts
+
+  get "store/index"
+
+
   resources :products
 
 
@@ -51,7 +58,7 @@ Depot::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'products#index'
+   root :to => 'store#index'
 
   # See how all your routes lay out with "rake routes"
 
